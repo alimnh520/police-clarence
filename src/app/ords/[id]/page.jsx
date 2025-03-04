35c3bc0c6a1124ba13531ace96ed4135b67268e9
𝓝𝓪𝓱𝓲𝓭 ☠️ <alimnh412@gmail.com>
2025-03-04 16:55:42 +0600

    add new code

--- src/app/ords/[id]/page.jsx
+++ src/app/ords/[id]/page.jsx
@@ -29,7 +29,7 @@ const page = () => {
     }
 
     return (
-        <div className={`w-full h-auto flex flex-col bg-[#e3e2e0] relative pb-[70px] main-contain`}>
+        <div className={`w-full h-auto flex flex-col text-black bg-[#e3e2e0] relative pb-[70px] main-contain`}>
             <div className='w-[81%] h-[88px] mt-[15px] border bg-white border-zinc-400 rounded-[2px] self-center flex items-center justify-start px-4 relative before:absolute before:w-full before:h-5 before:bg before:bottom-0 before:left-0 shadow-[0_2px_3px_#9b9a9a]'>
                 <div onClick={handlePrint}>
                     <img src='/printer_64.gif' alt='print' className='h-[50px]' />
@@ -37,9 +37,9 @@ const page = () => {
             </div>
 
             <div className="w-full flex justify-center" ref={contentRef}>
-                <div className='w-[725px] self-center mt-4 flex flex-col items-center justify-start'>
+                <div className='w-[725px] self-center mt-4 flex flex-col items-center justify-start mr-5'>
                     <img src='/bangladesh_govt_logo.png' className='h-[124px]' />
-                    <p className='text-[#393839] text-[27px] font-bold text-center mt-5 leading-[26px]'>GOVERNMENT OF THE PEOPLE'S REPUBLIC OF <br />
+                    <p className='text-[27px] font-bold text-center mt-5 leading-[26px]'>GOVERNMENT OF THE PEOPLE'S REPUBLIC OF <br />
                         BANGLADESH</p>
 
                     <div className='w-full h-40 flex justify-between items-start mt-3'>
@@ -51,16 +51,16 @@ const page = () => {
                             </div>
                             <p className="mt-1">Ref No. 1CHABXH</p>
                         </div>
-                        <div className="text-center text-lg text-[#393938] font-medium flex flex-col items-center justify-start gap-y-3 ml-8">
+                        <div className="text-center text-lg text-black font-medium flex flex-col items-center justify-start gap-y-3 ml-8">
                             <p>Adamdighi Police Station</p>
                             <p>Bogura</p>
                         </div>
-                        <p className="self-end">Dated: 20-FEB-2025</p>
+                        <p className="self-end">Dated: 25-FEB-2025</p>
                     </div>
                     <div className="w-fit mt-4 flex flex-col items-center justify-start">
-                        <p className="text-[#393938] text-[24px] font-bold">POLICE CLEARANCE CERTIFICATE</p>
+                        <p className="text-black text-[23px] font-bold">POLICE CLEARANCE CERTIFICATE</p>
 
-                        <p className="leading-7 text-lg font-normal mt-10 text-justify break-words">The character and antecedents of Mr. <span className="text-[#393938] font-bold">MD ZAKIR HOSSAIN</span> Son of <span className="text-[#393938] font-bold">MD SARAPAT ALI</span> Village/ Area: <span className="text-[#393938] font-bold">BIHIGRAM</span>, P/O: <span className="text-[#393938] font-bold">BIHIGRAM</span>, Post Code: <span className="text-[#393938] font-bold">5890</span>, P/S: <span className="text-[#393938] font-bold">Adamdighi</span>, District: <span className="text-[#393938] font-bold">Bogura</span> holder of Bangladesh International Passport No. <span className="text-[#393938] font-bold">A15807934</span> Issued at <span className="text-[#393938] font-bold">DIP/DHAKA</span> on <span className="text-[#393938] font-bold">24 FEB 2025</span> have been verified and there is no adverse information against him/her on record.</p>
+                        <p className="leading-7 text-lg font-normal mt-10 text-justify break-words">The character and antecedents of Mr. <span className="text-black font-bold">MD ZAKIR HOSSAIN</span> Son of <span className="text-black font-bold">MD SARAPAT ALI</span> Village/ Area: <span className="text-black font-bold">BIHIGRAM</span>, P/O: <span className="text-black font-bold">BIHIGRAM</span>, Post Code: <span className="text-black font-bold">5890</span>, P/S: <span className="text-black font-bold">Adamdighi</span>, District: <span className="text-black font-bold">Bogura</span> holder of Bangladesh International Passport No. <span className="text-black font-bold">A15807934</span> Issued at <span className="text-black font-bold">DIP/DHAKA</span> on <span className="text-black font-bold">24 MAY 2024</span> have been verified and there is no adverse information against him/her on record.</p>
 
                         <p className="mt-10 text-lg">This certificate is issued in pursuance of Ministry of Home Affairs Memo No. Nirdesh-2/75-Pt. 2152-Bohi(1), dated the 03th Mar, 1980.</p>
 
